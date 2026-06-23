6d991f0e9031d870ead7e0aa1f81d0e8fd7594ed
reval-legion <revaljonathan290307@gmail.com>
2026-06-23 20:23:52 +0700

    some

--- local-bin/backup.sh
+++ local-bin/backup.sh
@@ -7,7 +7,7 @@ cp  ~/.config/clipse/{theme.json,config.json} ~/code/val-niri/config/clipse/
 
 cp ~/.zshrc ~/.p10k.zsh ~/code/val-niri/
 
-cp ~/.local/bin/{wp.sh,tl.sh,filerofi.sh,powermenu.sh,sleep.sh,dynalock.sh,gamma.sh,dunstvol.sh,dunstback.sh,wifi.sh,steam.sh,browse.sh,barsel.sh,bat.sh,system.sh,weather.sh,ss.sh,backup.sh,wttrbar} ~/code/val-niri/local-bin/
+cp ~/.local/bin/{wp.sh,tl.sh,filerofi.sh,powermenu.sh,sleep.sh,dynalock.sh,gamma.sh,dunstvol.sh,dunstback.sh,wifi.sh,steam.sh,browse.sh,barsel.sh,bat.sh,mem.sh,weather.sh,ss.sh,backup.sh,wttrbar,player.sh} ~/code/val-niri/local-bin/
 
 cd ~/code/val-niri/
 
---
+++ local-bin/mem.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash 
+
+dunstify -u normal "System Status" "$(free -h | awk '/Mem:/ {print "RAM Usage: " $3 " / " $2}')" -a system -i ~/.config/dunst/ram.svg
