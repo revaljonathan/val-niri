152e54a49a06d28094456b23bbe8a1ed7160e54c
reval-legion <revaljonathan290307@gmail.com>
2026-06-25 20:20:07 +0700

    test only

--- config/nvim/after/plugin/matugen_colors.lua
+++ config/nvim/after/plugin/matugen_colors.lua
@@ -1,22 +1,22 @@
 local M = {}
 
-M.bg      = "#0f1416"
-M.fg      = "#dee3e6"
-M.primary = "#88d1ec"
-M.secondary = "#b3cad4"
+M.bg      = "#101418"
+M.fg      = "#e0e2e8"
+M.primary = "#9acbfa"
+M.secondary = "#b9c8da"
 M.error   = "#ffb4ab"
-M.cursorline_bg = "#40484c"
-M.cursorline_fg = "#bfc8cc"
+M.cursorline_bg = "#42474e"
+M.cursorline_fg = "#c2c7cf"
 
-M.telescope_border        = "#8a9296"
-M.telescope_prompt_border = "#88d1ec"
-M.telescope_prompt_title  = "#88d1ec"
-M.telescope_selection_bg  = "#004e61"
-M.telescope_selection_fg  = "#b7eaff"
+M.telescope_border        = "#8c9198"
+M.telescope_prompt_border = "#9acbfa"
+M.telescope_prompt_title  = "#9acbfa"
+M.telescope_selection_bg  = "#0c4a72"
+M.telescope_selection_fg  = "#cde5ff"
 
-M.tree_folder        = "#88d1ec"
-M.tree_folder_open   = "#c3c3eb"
-M.tree_indent_marker = "#40484c"
-M.tree_root          = "#b3cad4"
+M.tree_folder        = "#9acbfa"
+M.tree_folder_open   = "#d2bfe6"
+M.tree_indent_marker = "#42474e"
+M.tree_root          = "#b9c8da"
 
 return M
