d197032374ef93139e0a46ebd7300aa1c2041e3f
reval-legion <revaljonathan290307@gmail.com>
2026-09-21 20:47:16 +0700

    simpler aesthetic fix

--- config/zsh/export.zsh
+++ config/zsh/export.zsh
@@ -5,7 +5,7 @@ export "MICRO_TRUECOLOR=1"
 export EZA_CONFIG_DIR="$HOME/.config/eza/"
 export EDITOR=nvim
 export VISUAL=nvim
-export TERMINAL=kitty
+export TERMINAL="kitty"
 export MANPAGER="nvim +Man!"
 
 export PATH="$HOME/.cargo/bin:$PATH"
